8f0e15bbcb30947cc4a2293fca36d7e71807e612
EGNKupava <kupavaegn@gmail.com>
2021-05-28 20:04:41 +0300

    lession-5

--- src/components/card/card.jsx
+++ src/components/card/card.jsx
@@ -1,35 +1,32 @@
 import React from "react";
-
+import PropTypes from "prop-types";
 import styles from "./card.module.css";
 
 export class Card extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { counter: 0 };
-    this.onCounterChange = this.onCounterChange.bind(this);
-    this.onDescriptionChange = this.onDescriptionChange.bind(this);
+    this.state = { counter: 0, sumPrice: 0 };
   }
 
-  onCounterChange({ target: { value } }) {
+  onCounter = ({ target: { value } }) => {
     this.setState({
       counter: value,
+      sumPrice: value * this.props.cardData.price,
     });
-  }
+  };
 
-  onDescriptionChange({ target: { value } }) {
-    this.setState({
-      description: value,
-    });
-  }
+  onButtonClick = () => {
+    this.props.onBuy(this.props.cardData.id, +this.state.counter);
+    this.setState({ counter: 0 });
+  };
 
   render() {
-    console.log("props: ", this.props);
     const {
       cardData: { title, description, imgSrc },
     } = this.props;
     return (
       <div className={styles.card}>
-        <h2>{title}</h2>
+        <h2 className={styles.title}>{title}</h2>
         <div>{description}</div>
         <img src={imgSrc} alt="logo" className={styles.cardImg} />
         <div>
@@ -38,27 +35,25 @@ export class Card extends React.Component {
             <input
               type="number"
               name="card-counter"
-              onChange={this.onCounterChange}
+              onChange={this.onCounter}
               value={this.state.counter}
             />
           </label>
         </div>
-        <div>
-          <label>
-            Описание:
-            <input
-              type="text"
-              onChange={this.onDescriptionChange}
-              value={this.state.description}
-            />
-          </label>
-        </div>
-        <div>
-          <h4>В стейте записано: </h4>
-          <p> counter: {this.state.counter}</p>
-          <p> description: {this.state.description}</p>
-        </div>
+        <button type="button" onClick={this.onButtonClick}>
+          Добавить в корзину
+        </button>
       </div>
     );
   }
 }
+
+Card.defaultProps = {
+  cardData: {},
+  onBuy: () => {},
+};
+
+Card.propTypes = {
+  cardData: PropTypes.objectOf(PropTypes.any),
+  onBuy: PropTypes.func,
+};
