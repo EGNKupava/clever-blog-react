6454f441a0187778aa6cc224a7b0e708951eff64
EGNKupava <kupavaegn@gmail.com>
2021-06-09 10:28:03 +0300

    clock

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,22 +1,19 @@
 import React from "react";
 
+import { Clock } from "../clock";
+
 import logo from "./assets/logo.svg";
 
 import styles from "./header.module.css";
 
 export class Header extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { date: new Date() };
-  }
-
   render() {
     return (
       <div className={styles.header}>
         <div className={styles.logo}>
           <img src={logo} alt="картинка" />
         </div>
-        <h2>Компонет создан в {this.state.date.toLocaleTimeString()}.</h2>
+        <Clock />
       </div>
     );
   }
