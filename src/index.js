da0586df1c3526208d4048bcdf7d473405c20398
EGNKupava <kupavaegn@gmail.com>
2021-06-27 11:07:51 +0300

    Убрал renderAll

--- src/index.js
+++ src/index.js
@@ -4,26 +4,16 @@ import "./index.css";
 import { App } from "./components/app";
 import { BrowserRouter as Router } from "react-router-dom";
 import { Provider } from "react-redux";
-import reportWebVitals from "./reportWebVitals";
 
 import { store } from "./store/store";
 
-export const renderAll = () => {
-  ReactDOM.render(
-    <React.StrictMode>
-      <Provider store={store}>
-        <Router>
-          <App />
-        </Router>
-      </Provider>
-    </React.StrictMode>,
-    document.getElementById("root")
-  );
-};
-
-renderAll();
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <Router>
+        <App />
+      </Router>
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById("root")
+);
